2396b04541bfb849d5407f910809077b0ead9238
stéfany <stelar.dev@gmail.com>
2022-12-29 20:50:33 -0300

    connection to online server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://api-food-explorer.onrender.com",
-  // baseURL: "http://localhost:3000",
-  baseURL: "http://localhost:3002",
+  baseURL: "https://api-food-explorer.onrender.com",
+  // baseURL: "http://localhost:3002",
 });
